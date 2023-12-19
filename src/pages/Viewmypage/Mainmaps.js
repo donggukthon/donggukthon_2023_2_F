7339875cd532262bdba42008c5096eb04b0ad212
KyoungHyun <102707496+namsta9ramm@users.noreply.github.com>
2023-12-19 20:42:44 +0900

    info
    maininfopage

--- src/pages/Viewmypage/Mainmaps.js
+++ src/pages/Viewmypage/Mainmaps.js
@@ -2,39 +2,72 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import kakaoMapImage from '/Users/nam/Desktop/notmine/snowman1218/donggukthon_2023_2_F/src/box.png'; // 이미지 경로 설정
+var positions = [
+    {
+        title: '카카오', 
+        latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+    },
+    {
+        title: '생태연못', 
+        latlng: new kakao.maps.LatLng(33.450936, 126.569477)
+    },
+    {
+        title: '텃밭', 
+        latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+    },
+    {
+        title: '근린공원',
+        latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+    }
+];
+const Location = () => {
+    useEffect(() => {
+        var container = document.getElementById('map');
+        var options = {
+            center: new kakao.maps.LatLng(33.450705, 126.570677),
+            level: 10
+        };
+        var map = new kakao.maps.Map(container, options);
 
+        // 마커 이미지의 이미지 주소입니다
+        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
 
-const Location=()=>{
-    useEffect(()=>{
-    var container = document.getElementById('map');
-    var options = {
-        center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-        level: 3
-    };
-    var map = new kakao.maps.Map(container, options);
-    var markerPosition  = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488); 
-    var marker = new kakao.maps.Marker({
-        position: markerPosition
-    });
-    marker.setMap(map);
+        for (var i = 0; i < positions.length; i++) {
+            // 마커 이미지의 이미지 크기입니다
+            var imageSize = new kakao.maps.Size(30, 30);
+
+            // 마커 이미지를 생성합니다
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: positions[i].latlng,
+                title: positions[i].title,
+                image: markerImage
+            });
+
+            // 마커에 클릭 이벤트를 등록합니다
+            kakao.maps.event.addListener(marker, 'click', function () {
+                alert('마커 클릭됨');
+            });
+        }
     }, [])
-
 
     return (
         <div>
-        <div id="map" style={{ width: "405px", height: "400px" }}></div>
-        <div style={{ display: 'flex', alignItems: 'center',justifyContent: 'flex-end' }}>
-            <Link to='/main'>
-            <button style={{ marginBottom: '10px', color: 'red', fontWeight: 'bold' }}>
-                내 정보 보기
-            </button>
-            </Link>
-            <img src={kakaoMapImage} style={{ width: '140px', height: '50px' }} />
-
-        </div>
+            <div id="map" style={{ width: "405px", height: "400px" }}></div>
+            
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <Link to='/main'>
+                    <button style={{ marginBottom: '50px', color: 'red', fontWeight: 'bold' }}>
+                        내 정보 보기
+                    </button>
+                </Link>
+                <img src={kakaoMapImage} style={{ width: '150px', height: '50px' }} />
+            </div>
         </div>
     );
-
 }
 
 export default Location;
--- src/pages/Viewmypage/Mainpage.js
+++ src/pages/Viewmypage/Mainpage.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import snowman from '/Users/nam/Desktop/notmine/snowman1218/donggukthon_2023_2_F/src/image_32.png';
-import PostBtn from '../../components/PostBtn'
-
+import snowman from '/Users/nam/Desktop/notmine/snowman1218/donggukthon_2023_2_F/src/___________1.png';
+import PostBtn from '../../components/PostBtn';
+import window from '/Users/nam/Desktop/notmine/snowman1218/donggukthon_2023_2_F/src/____________5__1.png'
 const Hello = () => {
 return (
     <div style={{ textAlign: 'center' }}>
     <h1>어서와요 사용자님!</h1>
     <h1>만드신 눈사람은 0명이에요!</h1>
+    <br></br>
+    <br></br>
+    <br></br>
 
     <Link to='/see' style={{ marginBottom: '10px' }}>
-        <PostBtn value='내 눈사람 보기'/>
+        <PostBtn value='내 눈사람 보기' />
     </Link>
 
     <Link to='/commnet' style={{ marginBottom: '10px' }}>
@@ -27,12 +30,11 @@ return (
 
     {/* 이미지와 텍스트를 감싸는 div */}
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        
         <img
         src={snowman} // 이미지 파일의 경로를 지정해주세요.
-        alt="눈사람임"
-        style={{ width: '100%', maxWidth: '100px', margin: '10px' }}
+        style={{ width: '200%', maxWidth: '400px', margin: '5px' }}
         />
-        <p style={{ marginRight: '40px', fontSize: '18px' }}>오늘 목도리 어떠요?</p>
     </div>
     </div>
 );
